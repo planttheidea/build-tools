9f1873dab8807ddd18a4f4894f90700198bf5c43
Tony Quetano <tony.quetano@planttheidea.com>
2025-11-18 10:54:13 +0100

    ensure `format` api includes valid options

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,5 +1,23 @@
-import * as prettier from 'prettier';
+import { readFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import type { Config } from 'prettier';
+import { format as prettier } from 'prettier';
 
 export async function format(source: string, parser: 'json' | 'typescript' = 'typescript') {
-  return await prettier.format(source, { parser });
+  let optionsContent: string;
+
+  try {
+    // In case there are manual overrides in the existing file, use that.
+    optionsContent = await readFile(resolve(import.meta.dirname, '..', '..', '.prettierrc'), 'utf8');
+  } catch {
+    // If no existing file, use the template as it contains all defaults.
+    optionsContent = await readFile(
+      resolve(import.meta.dirname, '..', '..', 'templates', 'prettier', '.prettierrc'),
+      'utf8',
+    );
+  }
+
+  const options = JSON.parse(optionsContent) as Config;
+
+  return await prettier(source, { ...options, parser });
 }
